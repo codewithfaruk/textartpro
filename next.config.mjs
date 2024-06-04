2af4e2cf1c34c729d243e33705acbb9fc6d9a96e
Faruk <codewithfaruksardar@gmail.com>
2024-06-04 15:10:34 +0530

    redirect updated

--- next.config.mjs
+++ next.config.mjs
@@ -5,72 +5,72 @@ const nextConfig = {
       // Basic redirect
       {
         source: "/love-text-art.html",
-        destination: "/text-art/love-text-art",
+        destination: "/love-text-art",
         permanent: true,
       },
       {
         source: "/good-night-text-art.html",
-        destination: "/text-art/good-night-text-art",
+        destination: "/good-night-text-art",
         permanent: true,
       },
       {
         source: "/happy-birthday-text-art.html",
-        destination: "/text-art/happy-birthday-text-art",
+        destination: "/happy-birthday-text-art",
         permanent: true,
       },
       {
         source: "/happy-birthday-text-art.html",
-        destination: "/text-art/happy-birthday-text-art",
+        destination: "/happy-birthday-text-art",
         permanent: true,
       },
       {
         source: "/friendship-text-art.html",
-        destination: "/text-art/friendship-text-art",
+        destination: "/friendship-text-art",
         permanent: true,
       },
       {
         source: "/music-text-art.html",
-        destination: "/text-art/music-text-art",
+        destination: "/music-text-art",
         permanent: true,
       },
       {
         source: "/spongebob-text-art.html",
-        destination: "/text-art/spongebob-text-art",
+        destination: "/spongebob-text-art",
         permanent: true,
       },
       {
         source: "/animal-text-art.html",
-        destination: "/text-art/animal-text-art",
+        destination: "/animal-text-art",
         permanent: true,
       },
       {
         source: "/hello-hi-text-art.html",
-        destination: "/text-art/hello-hi-text-art",
+        destination: "/hello-hi-text-art",
         permanent: true,
       },
       {
         source: "/thumbs-up-text-art.html",
-        destination: "/text-art/thumbs-up-text-art",
+        destination: "/thumbs-up-text-art",
         permanent: true,
       },
       {
         source: "/thank-you-text-art.html",
-        destination: "/text-art/thank-you-text-art",
+        destination: "/thank-you-text-art",
         permanent: true,
       },
       {
         source: "/among-us-text-art.html",
-        destination: "/text-art/among-us-text-art",
+        destination: "/among-us-text-art",
         permanent: true,
       },
       {
         source: "/bunny-text-art.html",
-        destination: "/text-art/bunny-text-art",
+        destination: "/bunny-text-art",
         permanent: true,
       },
       {
         source: "/anime-text-art.html",
-        destination: "/text-art/anime-text-art",
+        destination: "/anime-text-art",
         permanent: true,
       },
       {
